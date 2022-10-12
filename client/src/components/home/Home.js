5bc3b9a504ec1428b9e718637d954912288d2490
ASFAND SHAMIM <khanasfand174@gmail.com>
2022-10-12 04:29:05 -0700

    added sliders

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,12 +1,15 @@
 import { Box } from "@mui/material";
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchProducts } from "../../features/product/productSlicer";
 import Navbar from "./navbar/Navbar";
+import ProductSlider from "./ProductSlider";
 import Slider from "./Slider";
 
 const Home = () => {
   const dispatch = useDispatch();
+
+  const { products } = useSelector((state) => state.product);
 
   useEffect(() => {
     dispatch(fetchProducts());
@@ -21,6 +24,14 @@ const Home = () => {
         }}
       >
         <Slider />
+      </Box>
+      <Box sx={{ padding: "0px 10px 10px 10px", backgroundColor: "#F2F2F2" }}>
+        <ProductSlider products={products} title="Deal of the day" />
+        <ProductSlider products={products} title="Monthly Essentials" />
+        <ProductSlider products={products} title="Home and kitchen" />
+        <ProductSlider products={products} title="Grooming and Books" />
+        <ProductSlider products={products} title="Fashion top deals" />
+        <ProductSlider products={products} title="Others" />
       </Box>
     </>
   );
---
+++ client/src/components/home/ProductSlider.js
@@ -0,0 +1,96 @@
+import { Box, Button, Divider, styled, Typography } from "@mui/material";
+import React from "react";
+import Carousel from "react-multi-carousel";
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 5,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 3,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
+
+const Image = styled("img")({
+  height: 150,
+  width: 150,
+  textAlign: "center",
+});
+
+const CarouselHeader = styled(Box)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+`;
+
+const CarouselSlider = styled(Carousel)`
+  margin-top: 10px;
+`;
+
+const Card = styled(Box)`
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ProductSlider = ({ products, title }) => {
+  console.log(products);
+  return (
+    <Box sx={{ background: "#ffffff", padding: "5px", margin: "10px 0px" }}>
+      <CarouselHeader>
+        <Typography>
+          <b>{title}</b>
+        </Typography>
+        <Button variant="contained">View All</Button>
+      </CarouselHeader>
+      <Divider />
+      <CarouselSlider
+        swipeable={false}
+        draggable={false}
+        responsive={responsive}
+        infinite={true}
+        autoPlay={true}
+        autoPlaySpeed={3000}
+        keyBoardControl={true}
+        customTransition="all .5"
+        transitionDuration={500}
+        containerClass="carousel-container"
+      >
+        {products.map((product, index) => (
+          <Box key={index}>
+            <Card
+              sx={{
+                textAlign: "center",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Image src={product.url} alt="product" />
+
+              <Typography sx={{ fontSize: "14px", fontWeight: "bold" }}>
+                {product.title.shortTitle}
+              </Typography>
+              <Typography sx={{ fontSize: "10px", color: "green" }}>
+                {product.discount}
+              </Typography>
+              <Typography sx={{ fontSize: "12px", color: "#9a9696" }}>
+                Rs {product.price.cost} /-
+              </Typography>
+            </Card>
+          </Box>
+        ))}
+      </CarouselSlider>
+    </Box>
+  );
+};
+
+export default ProductSlider;
